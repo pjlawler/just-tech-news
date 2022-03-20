c5e3c1d86668496b37e8142362be4ca0b2c2932a
Pat Lawler <patlaw777@icloud.com>
2022-03-20 12:29:49 -0500

    closes #3; completed authentication

--- contollers/home-routes.js
+++ contollers/home-routes.js
@@ -3,6 +3,9 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+
+  console.log(req.session);
+
  Post.findAll({
    attributes: [
      'id',
@@ -35,6 +38,16 @@ router.get('/', (req, res) => {
    console.log(err);
    res.status.json(err);
  });
+});
+
+router.get('/login', (req, res) => {
+
+  if(req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const path = require('path');
 const express = require('express');
 const routes = require('./contollers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -9,9 +22,9 @@ const hbs = exphbs.create({});
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
