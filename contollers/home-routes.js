6622ba7fd7f1547af60855889c8d08f48d78dd7f
Pat Lawler <patlaw777@icloud.com>
2022-03-20 10:10:47 -0500

    closes #1; finished homepage

--- contollers/home-routes.js
+++ contollers/home-routes.js
@@ -1,7 +1,40 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+ Post.findAll({
+   attributes: [
+     'id',
+     'post_url',
+     'title',
+     'created_at',
+     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+   ],
+   include: [
+     {
+       model: Comment,
+       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+       include: {
+         model: User,
+        attributes: ['username']
+     }
+    },
+    {
+      model: User,
+      attributes: ['username']
+    }
+   ]
+ })
+ .then(dbPostData => {
+   // pass asingle post object into the homepage template
+   const posts = dbPostData.map(post => post.get({ plain: true }));
+   res.render('homepage', { posts });
+ })
+ .catch(err => {
+   console.log(err);
+   res.status.json(err);
+ });
 });
 
 module.exports = router;
