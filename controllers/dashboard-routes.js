88bd8652c657ba127779f52596a96a931fc99fe7
Pat Lawler <patlaw777@icloud.com>
2022-03-20 22:48:52 -0500

    working

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,6 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
         where: {
@@ -84,5 +83,10 @@ router.get('/edit/:id', withAuth, (req, res) => {
         res.status(500).json(err);
       });
 })
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  
+});
+
 
 module.exports = router;
